642d561dcd7cb1d9aa5f0455c52465764983b4fc
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2018-05-10 20:58:21 -0300

    Update matriz.h

--- matriz.h
+++ matriz.h
@@ -2,24 +2,25 @@
 #define MATRIZ_H
 
 
+
 class matriz
 {
 private:
-	int **mat;
+	int ** mat;
 	int ql,qc,l,c;
-	
+
 public:
 	matriz(int nl, int nc);
 	void lermat();
 	void impmat();
-	/*bool somamat(matriz x);
-	bool subtmat(matriz x);
-	bool multmat(matriz x);
+	bool somamat(matriz x ,  matriz y);
+	bool subtmat(matriz x , matriz y);
+	bool multmat(matriz x , matriz y);
 	bool trisup();
 	bool triinf();
 	bool matsim();
-	bool matantsim();
-	*/ 
+	//bool matantsim();
+
 	~matriz();
 
 };
